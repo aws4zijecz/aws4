0223e1c7112cd92ba4aef63876e21f80b561c54b
Heinrich, Pavel <pavel.heinrich@oracle.com>
2024-02-26 21:34:19 +0100

    add s3 bucket encryption

--- step_1.tf
+++ step_1.tf
@@ -13,3 +13,14 @@ resource "aws_s3_bucket_versioning" "aws4-bucket4-versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse_config" {
+  bucket = aws_s3_bucket.aws4-bucket4.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.kms_key_1.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
