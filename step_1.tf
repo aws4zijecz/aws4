73fec1a598eb28100ff474b2a0e30d46fdf936c0
Heinrich, Pavel <pavel.heinrich@oracle.com>
2024-02-26 20:23:32 +0100

    split main.tf

--- step_1.tf
+++ step_1.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-    region = "eu-north-1"
-}
-
 resource "aws_kms_key" "kms_key_1" {
     description             = "KMS key for S3 bucket encryption"
     deletion_window_in_days = 10
