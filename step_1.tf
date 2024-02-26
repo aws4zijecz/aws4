a2b8ed167b3eef94263c2b4cc67b98c45860ea9d
Heinrich, Pavel <pavel.heinrich@oracle.com>
2024-02-26 21:31:50 +0100

    indent tf files

--- step_1.tf
+++ step_1.tf
@@ -1,4 +1,4 @@
 resource "aws_kms_key" "kms_key_1" {
-    description             = "KMS key for S3 bucket encryption"
-    deletion_window_in_days = 10
+  description             = "KMS key for S3 bucket encryption"
+  deletion_window_in_days = 10
 }
