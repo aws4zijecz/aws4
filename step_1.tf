d407f17adbda2f827647b7464f67f917c716c8b8
Heinrich, Pavel <pavel.heinrich@oracle.com>
2024-02-26 21:33:11 +0100

    add s3 bucket

--- step_1.tf
+++ step_1.tf
@@ -2,3 +2,7 @@ resource "aws_kms_key" "kms_key_1" {
   description             = "KMS key for S3 bucket encryption"
   deletion_window_in_days = 10
 }
+
+resource "aws_s3_bucket" "aws4-bucket4" {
+  bucket = "aws4-bucket4"
+}
