70aecf305c86a78dc3a6e2d1667d37c65636466a
Heinrich, Pavel <pavel.heinrich@oracle.com>
2024-03-04 23:38:38 +0100

    add step 5 - cloudformation

---
+++ step_5.formation.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudformation_stack" "superset_cf" {
+  name         = "superset-cf"
+  capabilities = ["CAPABILITY_IAM"]
+  parameters = {
+    SuperSetUserName      = "supersetuser"
+    SuperSetUserPassword  = "SuperSet123@"
+    WithExample           = "no"
+    InstallProphet        = "no"
+    ClusterName           = "superset-cf"
+    PublicSubnet1         = module.vpc.public_subnets[0]
+    PublicSubnet2         = module.vpc.public_subnets[1]
+    PrivateSubnet1        = module.vpc.private_subnets[0]
+    PrivateSubnet2        = module.vpc.private_subnets[1]
+    Vpc                   = module.vpc.vpc_id
+    SelfSignedCertificate = local.certificate_data.CertificateArn
+  }
+  template_url = "https://aws4-bucket5.s3.eu-north-1.amazonaws.com/formation/superset-entrypoint-existing-vpc.template.yaml"
+}
