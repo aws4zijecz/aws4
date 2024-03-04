521969bb9abedb0ef76ea0bc7c97c40dcb8ea773
Heinrich, Pavel <pavel.heinrich@oracle.com>
2024-03-04 23:48:17 +0100

    add locals to provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,19 @@
 provider "aws" {
-  region = "eu-north-1"
+  region = local.aws_region
+}
+
+locals {
+  name       = "superset"
+  aws_region = "eu-north-1"
+  account_id = "637423491198"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  tags = {
+    Name = local.name
+  }
+
+  # Load certificate ARN from the JSON file
+  certificate_data = jsondecode(file("certificate.json"))
 }
